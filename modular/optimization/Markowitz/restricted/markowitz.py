d797684b496eac4607892fd25bd2dbc0bf31ec7e
PraxedisJRuv <praxjruv@gmail.com>
2026-06-13 11:05:40 -0600

    fix: typing mistake in estimate.py

--- modular/optimization/Markowitz/restricted/markowitz.py
+++ modular/optimization/Markowitz/restricted/markowitz.py
@@ -1,9 +1,9 @@
 import markowitz_cpp
 import numpy as np
 
-def markowitz(Sigma: np.ndarray, wb:list[float], w_prev:np.ndarray, lambda_turnover:float):
+def markowitz(Sigma: np.ndarray, wb:list[float], w_prev:np.ndarray, lambda_turnover:float)->list[float]:
     """
-    This function is an intermiedate between the pybind module made for markowitz.cpp,
+    An intermiedate between the pybind module made for markowitz.cpp,
     so it can be implemented as a python function and imported as a module.
     """
     w_opt = markowitz_cpp.optimize_portfolio(Sigma, wb, w_prev, lambda_turnover)
